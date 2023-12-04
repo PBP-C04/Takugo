e0d7a7b4a0c0514e3c752523cd571002c27a7505
MightyZanark <40352213+MightyZanark@users.noreply.github.com>
2023-12-04 15:51:25 +0700

    add corsheaders

--- Takugo/settings.py
+++ Takugo/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'main',
     'books',
     'journal',
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Takugo.urls'
