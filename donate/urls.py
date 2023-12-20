5291629f74ea3547e73fa10b2f5dd752f35e50dc
fauzanjaisyu <fauzanjaisyu@gmail.com>
2023-12-20 22:14:16 +0700

    update views.py and urls.py

--- donate/urls.py
+++ donate/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('add_donate/', add_donate, name="add_donate"),
     path('show_donate_user/', show_donate_user, name="show_donate_user"),
     path('show_donate_lembaga/', show_donate_lembaga, name="show_donate_lembaga"),
+    path('show_json_donate/', show_json_donate, name="show_json_donate"),
+    path('show_json/', show_json, name="show_json"),
+    path('add_donate_flutter/', add_donate_flutter, name="add_donate_flutter"),
+    path('endpoint_for_books/', endpoint_for_books, name='endpoint_for_books'),
+    path('endpoint_for_lembaga/', endpoint_for_lembaga, name='endpoint_for_donate'),
 ]
